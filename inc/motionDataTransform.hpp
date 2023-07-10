1f7c1cabc9eaa0942ff24258a4ff8451ac48a32c
91418 <91418249@qq.com>
2023-07-10 18:20:44 +0800

    添加了静态函数，用于数据处理

--- inc/motionDataTransform.hpp
+++ inc/motionDataTransform.hpp
@@ -10,24 +10,39 @@
 #include "windows.h"
 #include "Driver.h"
 using namespace std;
-using mdt = class motionDataTransform{
+using MDT = class motionDataTransform{
 public:
-   static vector<float> getAngles(Driver d,const vector<DFS>& getData){
+   static vector<float> getAngles(Driver& d,const vector<DFS>& getData){
         vector<float> result{};
         int i{};
         for(auto g:getData){
-            result.push_back(g.Actual_Pos*(d._driver_gearRatioScalar[i]));
+            result.push_back(g.Actual_Pos/d._driver_gearRatioScalar[i]);
             i++;
-            cout<<d._driver_gearRatioScalar[i-1]<<",";
         }
         return result;
     }
-    static void fromAnglesToPulses(Driver d,const vector<float>& angles, vector<DTS>&SendData){
+    static void fromAnglesToPulses(Driver& d,const vector<float>& angles, vector<DTS>&SendData){
        int i{};
        for(auto &s:SendData){
             s.Target_Pos = angles[i]*d._driver_gearRatioScalar[i];
             i++;
        }
+    }
+    static vector<float>getMoments(Driver& d,const vector<DFS>&getData){
+       vector<float> result{};
+       for(auto g:getData){
+            result.push_back(g.Actual_Torque/16383*1.21/1000);
+       }
+       return result;
+    }
+    static vector<float>getVecs(Driver& d,const vector<DFS>&getData){
+      vector<float> result{};
+      int i{};
+      for(auto g:getData){
+            result.push_back(g.Actual_Vec/d._driver_gearRatioScalar[i]);
+            i++;
+      }
+      return result;
     }
 private:
     shared_ptr<vector<float>> gearRatio= nullptr;
