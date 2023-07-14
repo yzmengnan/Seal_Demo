6cc1c718acc60d1a4345dc0857156c16606dc727
yangqiang <91418249@qq.com>
2023-07-14 13:21:22 +0800

    删除了不必要的函数

--- src/MONITOR.cpp
+++ src/MONITOR.cpp
@@ -10,17 +10,13 @@
  */
 #include "MONITOR.h"
 #include <sstream>
-mutex th_mutex;
-void mt::tc(shared_ptr<bool> flag, int breakout_value) {
-    // std::cout << "Timer counting" << std::endl;
-    std::this_thread::sleep_for(std::chrono::milliseconds(breakout_value));
-    *flag = false;
-}
-void MONITOR::print_info(Tc_Ads &ads,const string& name) {
-    bool enableFlag=true;
-    vector<DFS>GetData(servoNUMs);
+#include <string>
+#include "motionDataTransform.hpp"
+void MONITOR::print_info(Driver d, const string &name) {
+    bool enableFlag = true;
+    vector<DFS> GetData(servoNUMs);
     SetConsoleOutputCP(CP_UTF8);
-    cout<<"创建管道！"<<endl;
+    cout << "创建管道！" << endl;
     SECURITY_ATTRIBUTES saAttr;
     saAttr.nLength = sizeof(SECURITY_ATTRIBUTES);
     saAttr.bInheritHandle = TRUE;
@@ -33,27 +29,27 @@ void MONITOR::print_info(Tc_Ads &ads,const string& name) {
     if (!ret)
     {
         cout << "创建管道失败：create pipe fail" << endl;
-        enableFlag=false;
+        enableFlag = false;
     }
 
     //设置写句柄不可以被子进程继承,不设置也不影响。 Ensure the write handle to the pipe for STDIN is not inherited.
     if (!SetHandleInformation(handle_write, HANDLE_FLAG_INHERIT, 0))
     {
         cout << "设置句柄失败：set handle fail!" << endl;
-        enableFlag=false;
+        enableFlag = false;
     }
     Multi_Process p;
-    if(p.monitor(name,handle_read)!=0){
-        enableFlag=false;
-        cout<<"监视进程创建失败！"<<endl;
+    if (p.monitor(name, handle_read) != 0) {
+        enableFlag = false;
+        cout << "监视进程创建失败！" << endl;
     }
     this_thread::sleep_for(chrono::seconds(2));
-    cout<<"监视器开启！"<<endl;
+    cout << "监视器开启！" << endl;
     DWORD len;
     while (enableFlag) {
-        auto servoData_success = ads.get(GetData);
-        if(servoData_success<0){
-            cout<<"Get Monitor Data error! Ads error: "<<servoData_success<<endl;
+        auto servoData_success = d.GetDataUpdate(GetData);
+        if (servoData_success < 0) {
+            cout << "Get Monitor Data error! Ads error: " << servoData_success << endl;
             ExitProcess(1);
         }
         string monitor_DATA{};
@@ -64,25 +60,22 @@ void MONITOR::print_info(Tc_Ads &ads,const string& name) {
                                    - std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch()).count() * 1000;
         time_t tt = std::chrono::system_clock::to_time_t(now);
         auto time_tm = localtime(&tt);
-        char strTime[25]={};
+        char strTime[25] = {};
         sprintf(strTime, "%d-%02d-%02d %02d:%02d:%02d %03d", time_tm->tm_year + 1900,
                 time_tm->tm_mon + 1, time_tm->tm_mday, time_tm->tm_hour,
-                time_tm->tm_min, time_tm->tm_sec, (int)dis_millseconds);
+                time_tm->tm_min, time_tm->tm_sec, (int) dis_millseconds);
 
-        monitor_DATA = "[ "+string{strTime}+" ]";
-        auto fadd_space=[](int16_t data){
-            auto str=[](int16_t data){
-                //如何引入motionDataTransform?
-                string ss = to_string((double)data/0x7fff*1.21*0.001);
-                return ss.substr(0,ss.find(".")+4);
-            };
-            string s2=str(data);
-            while(s2.size()<6)
-            s2 = " "+s2;
-            return s2+" ";
+        monitor_DATA = "[ " + string{strTime} + " ]";
+        auto fadd_space = [](float data) {
+            string s = to_string(data);
+            s = s.substr(0,s.find('.')+4);
+            while(s.size()<10)
+                s = " "+s;
+            return s;
         };
+        auto moms = MDT::getMoments(d,GetData);
         for(int i=0;i<servoNUMs;i++){
-            monitor_DATA+=fadd_space(GetData[i].Actual_Torque);
+            monitor_DATA+=fadd_space(moms[i]);
         }
         WriteFile(handle_write,monitor_DATA.data(),monitor_DATA.size(),&len,NULL);
         this_thread::sleep_for(chrono::milliseconds(10));
