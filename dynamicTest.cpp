b1f0a1a0b39bdb1c72558e7849558661b9ad99d1
yangqiang <91418249@qq.com>
2023-07-15 17:05:36 +0800

    测试

--- dynamicTest.cpp
+++ dynamicTest.cpp
@@ -9,11 +9,27 @@ int main(int argc, char const *argv[]) {
     Tc_Ads ads;
     shared_ptr<MotionV1> ptr_dev = make_shared<MotionV1>(ads);
     ptr_dev->Enable();
+    vector<DTS> senddata{ptr_dev->getSendData()};
+    vector<DFS> getdata(servoNUMs);
+//    ptr_dev->servoCST(senddata,getdata);
+        ptr_dev->servoCSP(senddata,getdata);
+    int nums{};
+    while(nums<50){
+        senddata[0].Target_Pos-=1200000;
+//        Sleep(100);
+//        senddata[0].Target_Pos=-900;
+        Sleep(3);
+        nums++;
+    }
+    Sleep(2000);
+    while(nums>0){
+        senddata[0].Target_Pos+=1100000;
+        //        Sleep(100);
+        //        senddata[0].Target_Pos=-900;
+        Sleep(3);
+        nums--;
+    }
+    ptr_dev->Disable();
     Sleep(1000);
-    auto d  = ptr_dev->getSendData();
-    d[0].Mode_of_Operation=10;
-    d[0].Target_Torque=-100;
-    ads.set(d);
-    Sleep(6000);
     return 0;
 }
