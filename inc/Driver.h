3931badb5456b95c6368e410d416d1e04f5f2a5b
yangqiang <91418249@qq.com>
2023-07-15 17:10:14 +0800

    测试

--- inc/Driver.h
+++ inc/Driver.h
@@ -11,6 +11,7 @@
 #include "jointDataProcess.h"
 #include <chrono>
 #include <iostream>
+#include <memory>
 #include <mutex>
 #include <numeric>
 #include <string>
@@ -36,49 +37,68 @@ public:
             i++;
         }
         int err = p_ads->set(SendData);
-        if(err<0){
-            cout<<"set profile velocity error!"<<err<<endl;
+        if (err < 0) {
+            cout << "set profile velocity error!" << err << endl;
         }
     }
-    void setGearRatioScalar(initializer_list<float> r){
+    void setGearRatioScalar(initializer_list<float> r) {
         int i{};
-        for(auto scalar:r){
+        for (auto scalar: r) {
             i++;
-            if(i>servoNUMs){
+            if (i > servoNUMs) {
                 break;
             }
-           _driver_gearRatioScalar[i-1]=scalar;
+            _driver_gearRatioScalar[i - 1] = scalar;
         }
     }
-    int GetDataUpdate(vector<DFS>&GetData){
-       th_mutex.lock();
-       auto err = p_ads->get(GetData);
-       th_mutex.unlock();
-       if(err<0){
-           cout<<"Get Data Update error :"<<err<<endl;
-           return err;
-       }
-       return 0;
+    int GetDataUpdate(vector<DFS> &GetData) {
+        th_mutex.lock();
+        auto err = p_ads->get(GetData);
+        th_mutex.unlock();
+        if (err < 0) {
+            cout << "Get Data Update error :" << err << endl;
+            return err;
+        }
+        return 0;
     }
     /**
      * @description: PP运动驱动程序,动作例1,执行点到点单独运动
      */
     auto servoPP0(std::vector<DTS> &SendData, std::vector<DFS> &GetData) -> int;
+    auto servoCST(vector<DTS> &SendData, vector<DFS> &GetData) -> int;
+    auto servoCSP(vector<DTS> &SendData, vector<DFS> &GetData) -> int;
+
     virtual ~Driver();
 
-
-
     //Test part
-    void TestBREAK(const bool& state){
-       servoBreak(state);
+    void TestBREAK(const bool &state) {
+        servoBreak(state);
     }
 
 private:
     auto servoBreak(const bool &state) -> int;
-    bool pp_Flag = 0;//=1表示pp就位，=0表示未就位
+    bool pp_Flag = false; //=1表示pp就位，=0表示未就位
+    bool cst_Flag = false;// 1 ready, 0 not ready
+    bool csp_Flag = false;// 1 ready, 0 not ready
+    shared_ptr<bool> cyclicFlag = make_shared<bool>(false) ;
     pTc_Ads p_ads = nullptr;
-    bool *const servoLag_flag = new bool(true);
     int error_code = 0;
+    void f_Cyclic(vector<DTS> &SendData) {
+        cout << "Cyclic START!" << endl;
+        while (*cyclicFlag) {
+            for(auto s:SendData){
+                cout<<"data: "<<s.Target_Pos<<",";
+            }
+            cout<<endl;
+            p_ads->set(SendData);
+            this_thread::sleep_for(chrono::milliseconds(10));
+        }
+        cout << "Cyclic QUIT!" << endl;
+    }
+    void servoFinishCS(){
+        *cyclicFlag =false;
+    }
+
 };
 
 class MotionV1 : public Driver {
@@ -86,7 +106,7 @@ public:
     MotionV1(Tc_Ads &ads_handle);
     int Enable();
     int Disable();
-    template<typename T,typename ...T2>
+    template<typename T, typename... T2>
     /*!
      *
      * @tparam T
@@ -103,38 +123,37 @@ public:
      * @param args
      * @return
      */
-    int Write(T operationMode='0',T2...args ){
-       //update target position with gearRatio_Scalar anyway!
+    int Write(T operationMode = '0', T2... args) {
+        //update target position with gearRatio_Scalar anyway!
         MotSendData = gearRatio_Scalar({args...});
         if (operationMode == '0') {
             // Normal motion with no sync-vec and no target change immediately
-            int err = servoPP0(MotSendData,MotGetData);
-            if(err<-1){
-                cout<<"MotionV1 : pp0 error"<<err<<endl;
+            int err = servoPP0(MotSendData, MotGetData);
+            if (err < -1) {
+                cout << "MotionV1 : pp0 error" << err << endl;
                 return err;
             }
         } else if (operationMode == '1') {
             //motion with sync-vec
             //get the servo data
             //canceled since the data is updating background
-//            auto err = GetDataUpdate(MotGetData);
-//            if(err<0){
-//                cout<<"MotionV1 Write error! "<< err<<endl;
-//                return err;
-//            }
+            //            auto err = GetDataUpdate(MotGetData);
+            //            if(err<0){
+            //                cout<<"MotionV1 Write error! "<< err<<endl;
+            //                return err;
+            //            }
             vector<uint32_t> Delta{};
-            for(int i=0;i<servoNUMs;i++){
-                Delta.push_back(abs(MotSendData[i].Target_Pos-MotGetData[i].Actual_Pos));
+            for (int i = 0; i < servoNUMs; i++) {
+                Delta.push_back(abs(MotSendData[i].Target_Pos - MotGetData[i].Actual_Pos));
             }
-            uint32_t maxDelta = *max_element(Delta.begin(),Delta.end());
+            uint32_t maxDelta = *max_element(Delta.begin(), Delta.end());
             //calculate and update each joint`s velocity
-            for(int vec_index=0;vec_index<servoNUMs;vec_index++){
-                MotSendData[vec_index].Profile_Velocity = sync_rpm*((float)(Delta[vec_index]/maxDelta))
-                                                          *this->_driver_gearRatioScalar[vec_index]*6;
+            for (int vec_index = 0; vec_index < servoNUMs; vec_index++) {
+                MotSendData[vec_index].Profile_Velocity = sync_rpm * ((float) (Delta[vec_index] / maxDelta)) * this->_driver_gearRatioScalar[vec_index] * 6;
             }
-            auto err = servoPP0(MotSendData,MotGetData);
-            if(err<0){
-                cout<<"MotionV1 :pp1 error"<<err<<endl;
+            auto err = servoPP0(MotSendData, MotGetData);
+            if (err < 0) {
+                cout << "MotionV1 :pp1 error" << err << endl;
                 return err;
             }
 
@@ -148,28 +167,28 @@ public:
         }
         return -1;
     }
-    template<typename...T>
-    void setProfileVelocity(T...args){
+    template<typename... T>
+    void setProfileVelocity(T... args) {
         vector<float> dps;
-        for(auto dps_k:{args...}){
+        for (auto dps_k: {args...}) {
             dps.push_back(dps_k);
         }
-        this->Driver::setProfileVelocity(dps,this->MotSendData);
+        this->Driver::setProfileVelocity(dps, this->MotSendData);
     }
-    virtual ~MotionV1();
+    ~MotionV1() final;
     /*!
      * @Description 注意，设置同步速度时，考虑不同轴减速比不同，无法定义同步转速，
      *              因此选取单位为rpm，表达最高轴的伺服轴输出转速
      * @param rpm
      */
-    void setSyncrpm(int rpm){
+    void setSyncrpm(int rpm) {
         this->sync_rpm = rpm;
     }
     /*!
      * @details 返回MotionV1内部的发送数据
      * @return
      */
-    vector<DTS> getSendData(){
+    vector<DTS> getSendData() {
         return this->MotSendData;
     }
 
