7fa3950816a1063f320665ded79e41ba5239d634
Yang <914181249@qq.com>
2023-07-18 13:06:27 +0800

    使用字符串流处理管道数据

--- src/MONITOR.cpp
+++ src/MONITOR.cpp
@@ -9,9 +9,9 @@
  * Copyright (c) 2023 by YangQ, All Rights Reserved.
  */
 #include "MONITOR.h"
+#include "motionDataTransform.hpp"
 #include <sstream>
 #include <string>
-#include "motionDataTransform.hpp"
 void MONITOR::print_info(Driver d, const string &name) {
     bool enableFlag = true;
     vector<DFS> GetData(servoNUMs);
@@ -26,15 +26,13 @@ void MONITOR::print_info(Driver d, const string &name) {
     HANDLE handle_read;
     HANDLE handle_write;
     bool ret = CreatePipe(&handle_read, &handle_write, &saAttr, 0);
-    if (!ret)
-    {
+    if (!ret) {
         cout << "创建管道失败：create pipe fail" << endl;
         enableFlag = false;
     }
 
     //设置写句柄不可以被子进程继承,不设置也不影响。 Ensure the write handle to the pipe for STDIN is not inherited.
-    if (!SetHandleInformation(handle_write, HANDLE_FLAG_INHERIT, 0))
-    {
+    if (!SetHandleInformation(handle_write, HANDLE_FLAG_INHERIT, 0)) {
         cout << "设置句柄失败：set handle fail!" << endl;
         enableFlag = false;
     }
@@ -52,33 +50,38 @@ void MONITOR::print_info(Driver d, const string &name) {
             cout << "Get Monitor Data error! Ads error: " << servoData_success << endl;
             ExitProcess(1);
         }
-        string monitor_DATA{};
 
         auto now = std::chrono::system_clock::now();
         //通过不同精度获取相差的毫秒数
-        uint64_t dis_millseconds = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count()
-                                   - std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch()).count() * 1000;
+        uint64_t dis_millseconds = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()).count() - std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch()).count() * 1000;
         time_t tt = std::chrono::system_clock::to_time_t(now);
         auto time_tm = localtime(&tt);
         char strTime[25] = {};
         sprintf(strTime, "%d-%02d-%02d %02d:%02d:%02d %03d", time_tm->tm_year + 1900,
                 time_tm->tm_mon + 1, time_tm->tm_mday, time_tm->tm_hour,
                 time_tm->tm_min, time_tm->tm_sec, (int) dis_millseconds);
-
-        monitor_DATA = "[ " + string{strTime} + " ]";
+        ostringstream message;
+        message<<"["<<string(strTime)<<"]";
         auto fadd_space = [](float data) {
             string s = to_string(data);
-            s = s.substr(0,s.find('.')+4);
-            while(s.size()<10)
-                s = " "+s;
+            s = s.substr(0, s.find('.') + 4);
+            while (s.size() < 10)
+                s = " " + s;
             return s;
         };
-        auto moms = MDT::getMoments(d,GetData);
-        for(int i=0;i<servoNUMs;i++){
-            monitor_DATA+=fadd_space(moms[i]);
+        auto angs = MDT::getAngles(d,GetData);
+        auto vecs = MDT::getVecs(d,GetData);
+        auto moms = MDT::getMoments(d, GetData);
+        for(int i =0;i<3;i++) {
+            for (int j = 0; j < servoNUMs; j++) {
+                message<<fadd_space(moms[i]);
+            }
+            message<<"---";
         }
-        WriteFile(handle_write,monitor_DATA.data(),monitor_DATA.size(),&len,NULL);
+        message <<"!"<<endl;
+        auto message_data = message.str();
+        WriteFile(handle_write, message_data.data(), message_data.size(), &len, NULL);
         this_thread::sleep_for(chrono::milliseconds(10));
     }
-    cout<<"后台线程数据显示进程启动失败！"<<endl;
+    cout << "后台线程数据显示进程启动失败！" << endl;
 }
