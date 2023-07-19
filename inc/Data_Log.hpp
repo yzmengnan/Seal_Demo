6b9f21328b668c4c7fffa5e39410dee0fa5943e5
yangqiang <91418249@qq.com>
2023-07-19 17:08:02 +0800

    设置参数为MotionV1的引用 使用TimerCounter进行精确计时

--- inc/Data_Log.hpp
+++ inc/Data_Log.hpp
@@ -14,68 +14,63 @@
 using namespace std;
 using file_log = class Data_Log {
 public:
+    void finishWrite() {
+        cout << "finish record!" << endl;
 
-    void finishWrite(){
-        cout<<"finish record!"<<endl;
-
-        this->writeEnable=false;
+        this->writeEnable = false;
     }
-    void writeFile(Driver d) {
-        vector<DFS> getData(servoNUMs);
-        SYSTEMTIME systime={0};
+    void writeFile(const MotionV1 &m) {
+        SYSTEMTIME systime = {0};
         GetSystemTime(&systime);
         stringstream file_name;
-        file_name<<systime.wMonth<<"-"<<systime.wDay<<"-"
-                  <<((systime.wHour+8)>24?(systime.wHour+8/24)+1:systime.wHour+8)
-                  <<"-"<<systime.wMinute;
-        fstream fp,fv,fm;
-        string file_address="../data/";
-        string file_position = file_address+file_name.str()+" angles.txt";
-        string file_vec =file_address+file_name.str()+" vecs.txt";
-        string file_moments = file_address+file_name.str()+" moments.txt";
-        fp.open(file_position,ios::out);
-        fv.open(file_vec,ios::out);
-        fm.open(file_moments,ios::out);
+        file_name << systime.wMonth << "-" << systime.wDay << "-"
+                  << ((systime.wHour + 8) > 24 ? (systime.wHour + 8 / 24) + 1 : systime.wHour + 8)
+                  << "-" << systime.wMinute;
+        fstream fp, fv, fm;
+        string file_address = "../data/";
+        string file_position = file_address + file_name.str() + " angles.txt";
+        string file_vec = file_address + file_name.str() + " vecs.txt";
+        string file_moments = file_address + file_name.str() + " moments.txt";
+        fp.open(file_position, ios::out);
+        fv.open(file_vec, ios::out);
+        fm.open(file_moments, ios::out);
         int index{};
-        if(writeEnable){
-            cout<<"start data record!"<<endl;
+        if (writeEnable) {
+            cout << "start data record!" << endl;
         }
-        while(this->writeEnable) {
-            //伺服器类内部有私有getData成员，后台线程持续刷新
-            //getData为保护内容，不设置对外访问权限，即驱动函数类内优先级最高。
-            //其他线程需要访问getData时，执行主动请求，且设置线程锁。
-            //调用参数为d，只访问基类对象
-                auto err = d.GetDataUpdate(getData);
-                if (err < 0) {
-                    cout << "log operation err!" << err << endl;
-                    break;
+        TimerCounter tc;
+        tc.Start();
+        while (this->writeEnable) {
+            if (tc.dbTime * 1000 > 10) {
+                tc.Start();
+                {
+                    stringstream data;
+                    data << index;
+                    for (auto d: MDT::getAngles(m, m.MotGetData)) {
+                        data << " " << d;
+                    }
+                    fp << data.str() << endl;
                 }
-            {
-                stringstream data;
-                data << index;
-                for (auto d: MDT::getAngles(d, getData)) {
-                    data<< " " << d;
+                {
+                    stringstream data;
+                    data << index;
+                    for (auto d: MDT::getVecs(m, m.MotGetData)) {
+                        data << " " << d;
+                    }
+                    fv << data.str() << endl;
                 }
-                fp<<data.str()<<endl;
+                {
+                    stringstream data;
+                    data << index;
+                    for (auto d: MDT::getMoments(m, m.MotGetData)) {
+                        data << " " << d;
+                    }
+                    fm << data.str() << endl;
+                }
+                index++;
             }
-            {
-                stringstream data;
-                data << index;
-                for (auto d: MDT::getVecs(d, getData)) {
-                    data << " " << d;
-                }
-                fv<<data.str()<<endl;
-            }
-            {
-                stringstream data;
-                data << index;
-                for (auto d: MDT::getMoments(d, getData)) {
-                    data << " " << d;
-                }
-                fm<<data.str()<<endl;
-            }
-            index++;
-            this_thread::sleep_for(chrono::milliseconds(20));
+            tc.Stop();
+            //            this_thread::sleep_for(chrono::milliseconds(10));
         }
         fp.close();
         fv.close();
@@ -89,7 +84,7 @@ private:
     vector<vector<float>> _angles{};
     vector<vector<float>> _moments{};
     vector<vector<float>> _velocities{};
-    bool writeEnable=true;
+    bool writeEnable = true;
 };
 
 
