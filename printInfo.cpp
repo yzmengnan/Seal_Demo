374356c0af8895dc937c2ee4c75fdb2acbb0a447
yangqiang <91418249@qq.com>
2023-07-19 13:17:45 +0800

    取消monitor进程5秒结束

--- printInfo.cpp
+++ printInfo.cpp
@@ -31,7 +31,6 @@ int main(int argc, char* argv[])
         Sleep(10);
     }
     cout << "finish reading!" << endl;
-    Sleep(5000);
     freopen("CON", "r", stdin);    // 重定向输入,否则system("pause")会无效
     //CloseHandle(handle_read);
 //    system("pause");
